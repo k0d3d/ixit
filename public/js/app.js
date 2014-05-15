c7bef3545b1bcd245847bfae7660bbd69f471c29
Mykel Koded <michael.rhema@gmail.com>
2014-05-15 23:08:51 +0100

    fixed progress bars for queued files

--- public/js/app.js
+++ public/js/app.js
@@ -40,7 +40,7 @@ app.config([
   // You can also set default events:
   flowFactoryProvider.on('catchAll', function (event) {
     // ...
-    console.log('catchAll', arguments);
+    // console.log('catchAll', arguments);
   });
     // Can be used with different implementations of Flow.js
     // flowFactoryProvider.factory = fustyFlowFactory;  
